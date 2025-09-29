07c867b8a9cd306e9a2b4362e2c66a41c9506ad3
Lili Syaripudin <lsyaripudin@gmail.com>
2025-09-29 13:45:14 +0700

    Redesign landing page around Google Docs content

--- models/home.go
+++ models/home.go
@@ -1,18 +1,20 @@
 package models
 
+// Home represents the localized content that powers the landing page.
 type Home struct {
-	Home1H1    string `json:"slide1H1"`
-	Home1H3    string `json:"slide1H3"`
-	Home2H1    string `json:"slide2H1"`
-	Home2H3    string `json:"slide2H2"`
-	Home3H1    string `json:"slide3H1"`
-	Home3H3    string `json:"slide3H2"`
-	MapHeader  string `json:"mapHeader"`
-	MapContent string `json:"mapContent"`
+	HeroTitle       string      `json:"heroTitle"`
+	HeroSubtitle    string      `json:"heroSubtitle"`
+	HeroButtonLabel string      `json:"heroButtonLabel"`
+	HeroSupport     string      `json:"heroSupport"`
+	DocURL          string      `json:"docUrl"`
+	DocEmbedURL     string      `json:"docEmbedUrl"`
+	DocNote         string      `json:"docNote"`
+	Highlights      []Highlight `json:"highlights"`
 }
 
-type GoogleSheetResponse struct {
-	Range          string     `json:"range"`
-	MajorDimension string     `json:"majorDimension"`
-	Values         [][]string `json:"values"`
+// Highlight represents a short value proposition displayed beneath the hero section.
+type Highlight struct {
+	Icon        string `json:"icon"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
 }
